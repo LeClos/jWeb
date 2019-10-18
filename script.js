c92630fef9218ae09d402a420918a52c1d46bf75
LeClos <jared.leclos@gmail.com>
2019-10-18 15:17:55 +0800

    thematic chages

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       {
         scrollTop: $(targetHref).offset().top
       },
-      1000
+      500
     );
 
     e.preventDefault();
@@ -20,7 +20,7 @@ $(document).ready(function() {
       {
         scrollTop: $(targetHref).offset().top
       },
-      1000
+      500
     );
 
     e.preventDefault();
@@ -28,29 +28,7 @@ $(document).ready(function() {
 
   // Home button fading in
   $("#home1").css("display", "none");
-  $("#home1").fadeIn(1000);
+  $("#home1").fadeIn(500);
 });
 
-// scroll to top
 
-//Get the button:
-mybutton = document.getElementById("scroll");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
